ef8c8bce4fcbd3288fec20d32866304e8310cc41
Nicklas Söderberg <nicklas.soderberg@hotmail.se>
2020-03-18 17:14:23 +0100

    Initial commit

---
+++ PizzaPalace/Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaPalace.Models
+{
+    class Pizza
+    {
+        public string Name {get; set;}
+        public double Price { get; set; }
+        public bool Vegan { get; set; }
+
+        public Pizza(string setName, double setPrice, bool setVegan)
+        {
+            Name = setName;
+            Price = setPrice;
+            Vegan = setVegan;
+        }
+    }
+
+    
+}
---
+++ PizzaPalace/ViewModels/PizzaViewModel.cs
@@ -0,0 +1,43 @@
+﻿using PizzaPalace.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace PizzaPalace.ViewModels
+{
+    class PizzaViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+
+        public ObservableCollection<Pizza> Pizzas { get; set; }
+
+
+        public PizzaViewModel()
+        {
+            Pizzas = new ObservableCollection<Pizza>();
+
+            Pizza TPizza = new Pizza("Kebab", 49.99, false);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Vezuvio", 54.99, false);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Mafia", 59.99, false);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Bolognese", 59.99, false);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Italia Special", 64.99, false);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Veggiepizza", 84.99, true);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Margarita", 44.99, false);
+            Pizzas.Add(TPizza);
+            TPizza = new Pizza("Calabrese", 74.99, false);
+            Pizzas.Add(TPizza);
+        }
+
+        
+    }
+}
